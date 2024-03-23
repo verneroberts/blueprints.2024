ad57e9751e00b2c7df3f463a42f2006310328b17
verneroberts <1232285+verneroberts@users.noreply.github.com>
2024-03-24 11:43:04 +1300

    settings from config!

--- src/main/java/vernando/imageref/ImageRefConfig.java
+++ src/main/java/vernando/imageref/ImageRefConfig.java
@@ -1,28 +1,23 @@
 package vernando.imageref;
-
-import com.google.common.collect.Lists;
 import eu.midnightdust.lib.config.MidnightConfig;
-import eu.midnightdust.lib.config.MidnightConfig.Entry;
-
-import java.util.List;
-
 
 public class ImageRefConfig extends MidnightConfig {
 
     public enum ShowHideEnum {
         SHOW, HIDE
     }
+    @Entry(category = "global", name = "Render Through Blocks") public static boolean renderThroughBlocks = false;
     @Entry(category = "global", name = "Show/Hide") public static ShowHideEnum show = ShowHideEnum.SHOW;
-    @Entry(category = "global", name = "Position X") public static double positionX = 0d;
-    @Entry(category = "global", name = "Position Y") public static double positionY = 0d;
-    @Entry(category = "global", name = "Position Z") public static double positionZ = 0d;
-    @Entry(category = "global", name = "Scale X") public static double scaleX = 1d;
-    @Entry(category = "global", name = "Scale Y") public static double scaleY = 1d;
-    @Entry(category = "global", name = "Scale Z") public static double scaleZ = 1d;
-    @Entry(category = "global", name = "Rotation X", isSlider = true, min = 0, max = 1f, precision = 1000) public static double rotationX = 0d;
-    @Entry(category = "global", name = "Rotation Y", isSlider = true, min = 0, max = 1f, precision = 1000) public static double rotationY = 0d;
-    @Entry(category = "global", name = "Rotation Z", isSlider = true, min = 0, max = 1f, precision = 1000) public static double rotationZ = 0d;
-    @Entry(category = "global", name = "Image Path") public static String imagePath = "./test.png"; 
+    @Entry(category = "global", name = "Position X") public static float positionX = 0f;
+    @Entry(category = "global", name = "Position Y") public static float positionY = 0f;
+    @Entry(category = "global", name = "Position Z") public static float positionZ = 0f;
+    @Entry(category = "global", name = "Scale X") public static float scaleX = 1f;
+    @Entry(category = "global", name = "Scale Y") public static float scaleY = 1f;
+    @Entry(category = "global", name = "Rotation X", isSlider = true, min = 0, max = 360f, precision = 1000) public static float rotationX = 0f;
+    @Entry(category = "global", name = "Rotation Y", isSlider = true, min = 0, max = 360f, precision = 1000) public static float rotationY = 0f;
+    @Entry(category = "global", name = "Rotation Z", isSlider = true, min = 0, max = 360f, precision = 1000) public static float rotationZ = 0f;
+    @Entry(category = "global", name = "Alpha", isSlider = true, min = 0, max = 1f, precision = 1000) public static float alpha = 1f;
+    @Entry(category = "global", name = "Image Path") public static String imagePath = "master-pnp-habshaer-fl-fl0700-fl0701-photos-577579pu.png"; 
     
 
     public static int imposter = 16777215; // - Entries without an @Entry or @Comment annotation are ignored
