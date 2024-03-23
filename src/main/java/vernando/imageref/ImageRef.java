242bf6c2f94719f3ef889d6bb8eb21ca363f1f5a
verneroberts <1232285+verneroberts@users.noreply.github.com>
2024-03-23 21:33:22 +1300

    thinking about config screen

--- src/main/java/vernando/imageref/ImageRef.java
+++ src/main/java/vernando/imageref/ImageRef.java
@@ -5,6 +5,8 @@ import net.fabricmc.api.ModInitializer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import eu.midnightdust.lib.config.MidnightConfig;
+
 public class ImageRef implements ModInitializer {
 	// This logger is used to write text to the console and the log file.
 	// It is considered best practice to use your mod id as the logger's name.
@@ -21,5 +23,6 @@ public class ImageRef implements ModInitializer {
 
 		// Register the config
 		ImageRefConfig.init("image-ref", ImageRefConfig.class);
+		// screen = MidnightConfig.getScreen(parent, "image-ref");
 	}
 }
--- src/main/java/vernando/imageref/ImageRefConfig.java
+++ src/main/java/vernando/imageref/ImageRefConfig.java
@@ -2,28 +2,28 @@ package vernando.imageref;
 
 import com.google.common.collect.Lists;
 import eu.midnightdust.lib.config.MidnightConfig;
+import eu.midnightdust.lib.config.MidnightConfig.Entry;
 
 import java.util.List;
 
 
 public class ImageRefConfig extends MidnightConfig {
-    @Comment(category = "text") public static Comment text1;                       // Comments are rendered like an option without a button and are excluded from the config file
-    @Comment(category = "text", centered = true) public static Comment text2;      // Centered comments are the same as normal ones - just centered!
-    @Comment(category = "text") public static Comment spacer1;                     // Comments containing the word "spacer" will just appear as a blank line
-    @Entry(category = "text") public static boolean showInfo = true;               // Example for a boolean option
-    @Entry(category = "text") public static String name = "Hello World!";          // Example for a string option, which is in a category!
-    @Entry(category = "text") public static TestEnum testEnum = TestEnum.FABRIC;   // Example for an enum option
-    public enum TestEnum {                               // Enums allow the user to cycle through predefined options
-        QUILT, FABRIC, FORGE
+
+    public enum ShowHideEnum {
+        SHOW, HIDE
     }
-    @Entry(category = "numbers") public static int fabric = 16777215;                 // Example for an int option
-    @Entry(category = "numbers") public static double world = 1.4D;                   // Example for a double option
-    @Entry(category = "numbers", min=69,max=420) public static int hello = 420;   // - The entered number has to be larger than 69 and smaller than 420
-    @Entry(category = "text", width = 7, min = 7, isColor = true, name = "I am a color!") public static String titleColor = "#ffffff"; // The isColor property adds a preview box for a hexadecimal color
-    @Entry(category = "text", name = "I am an array list!") public static List<String> arrayList = Lists.newArrayList("String1", "String2"); // Array String Lists are also supported
-    @Entry(category = "sliders", name = "I am an int slider.",isSlider = true, min = 0, max = 100) public static int intSlider = 35; // Int fields can also be displayed as a Slider
-    @Entry(category = "sliders", name = "I am a float slider!", isSlider = true, min = 0f, max = 1f, precision = 1000) public static float floatSlider = 0.24f; // And so can floats! Precision defines the amount of decimal places
-    // The name field can be used to specify a custom translation string or plain text
+    @Entry(category = "global", name = "Show/Hide") public static ShowHideEnum show = ShowHideEnum.SHOW;
+    @Entry(category = "global", name = "Position X") public static double positionX = 0d;
+    @Entry(category = "global", name = "Position Y") public static double positionY = 0d;
+    @Entry(category = "global", name = "Position Z") public static double positionZ = 0d;
+    @Entry(category = "global", name = "Scale X") public static double scaleX = 1d;
+    @Entry(category = "global", name = "Scale Y") public static double scaleY = 1d;
+    @Entry(category = "global", name = "Scale Z") public static double scaleZ = 1d;
+    @Entry(category = "global", name = "Rotation X", isSlider = true, min = 0, max = 1f, precision = 1000) public static double rotationX = 0d;
+    @Entry(category = "global", name = "Rotation Y", isSlider = true, min = 0, max = 1f, precision = 1000) public static double rotationY = 0d;
+    @Entry(category = "global", name = "Rotation Z", isSlider = true, min = 0, max = 1f, precision = 1000) public static double rotationZ = 0d;
+    @Entry(category = "global", name = "Image Path") public static String imagePath = "./test.png"; 
+    
 
     public static int imposter = 16777215; // - Entries without an @Entry or @Comment annotation are ignored
 }
